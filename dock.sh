849ba1224e02d3e0c7342bbb69ba8adf5f7e3393
mkalitka <mikolaj.kalitka@proton.me>
2025-04-10 12:31:41 +0200

    feat: add dock setting

--- dock.sh
+++ dock.sh
@@ -2,5 +2,6 @@
 
 defaults write com.apple.dock autohide-delay -int 0
 defaults write com.apple.dock scroll-to-open -bool true
+defaults write com.apple.dock springboard-page-duration -float .3
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
 killall Dock
